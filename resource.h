be19f0d612f0ee9acba0b6fede9689e4de40fd7f
Archangel35757 <archangel35757@yahoo.com>
2017-08-04 14:16:28 -0500

    Added ID_VIEW_VERTEXNORMALS to Dutch Accelerator

--- resource.h
+++ resource.h
@@ -82,7 +82,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40063
+#define _APS_NEXT_COMMAND_VALUE         40064
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
